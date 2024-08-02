3d2dafe79830ab29aa9b050df13f6251855f06d1
Anishakar_123 <143691489+moumi-123@users.noreply.github.com>
2024-08-02 11:21:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayImage(imageSrc) {
+    const imageElement = document.getElementById('displayedImage');
+    imageElement.src = imageSrc;
+    imageElement.style.display = 'block';
+}
+
